b1d233467638017a04bd01d6d7a9c83e916db3d3
Crawford Collins <crawford.collins@gmail.com>
2017-07-08 15:54:57 -0500

    pg 41

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,14 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @@moduledoc """
+  in memory repository
+  """
+  def all(rumbl.User) do
+    [%Rumbl.User{id: "1", name: "jose", username: "josevalim", password: "elixir"}]
+
+  end
+  def all(_module), do: []
+  def get(module, id) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+  end
 end
