c0f8b3f9481259fe09d864c0b2e318b6d17001f8
Crawford Collins <crawford.collins@gmail.com>
2017-07-08 16:20:03 -0500

    pg 46 errors

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,14 +1,19 @@
 defmodule Rumbl.Repo do
-  @@moduledoc """
+  @moduledoc """
   in memory repository
   """
-  def all(rumbl.User) do
+  def all(Rumbl.User) do
     [%Rumbl.User{id: "1", name: "jose", username: "josevalim", password: "elixir"}]
 
   end
   def all(_module), do: []
   def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
     Enum.find all(module), fn map ->
       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
   end
 end
